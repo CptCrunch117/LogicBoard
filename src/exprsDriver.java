183868578dc666e9cbecfd967c785d4c34ac8623
CptCrunch117 <hanbrolo117@gmail.com>
2015-07-02 19:04:40 -0700

    Added cool examples of what can be done in the exprsDriver

--- src/exprsDriver.java
+++ src/exprsDriver.java
@@ -8,15 +8,28 @@ public class exprsDriver {
 
     public static void main(String[] args){
 
+
+        //In 4 lines a boolean expression can be broken down, and auto build a logic board that satisfies the
+        // expression!
+
+            //Example 1: XOR gate
+            String xor_expression = "( ( A ' ) * B ) + ( A * ( B ' ) )";
+            String[] inputs = {"A", "B", "C"};
+            BoardTreeBuilder builder = new BoardTreeBuilder();
+            LogicBoard xor = builder.buildTree(xor_expression, inputs, "XOR");
+
+
+        //Adding another expression to the same board!
         String expression = "( ( A * B ) + ( A * B ) ) * C";
-        String xor_expression = "( ( A ' ) * B ) + ( A * ( B ' ) )";
-        String[] inputs = {"A", "B"};
-        BoardTreeBuilder builder = new BoardTreeBuilder();
-        LogicBoard xor = builder.buildTree(xor_expression, inputs, "XOR");
+        LogicBoard multExprsBoard = builder.buildTree(expression, xor);
+
+        //Print results of xor LogicBoard
         xor.printGates();
         System.out.println(xor.generateTruthTable());
 
-        //
+        //Print results of multExprsBoard LogicBoard
+        multExprsBoard.printGates();
+        System.out.println(multExprsBoard.generateTruthTable());
     }
 
 }
--- src/logicboard/boardbuilder/BoardTreeBuilder.java
+++ src/logicboard/boardbuilder/BoardTreeBuilder.java
@@ -8,6 +8,59 @@ import logicboard.LogicBoard;
 public class BoardTreeBuilder {
 
 
+    public LogicBoard buildTree(String exprs, LogicBoard bd){
+        BoardTree<String> tree = new BoardTree<String>(bd);
+        BinaryNode<String> temp = tree.root;
+
+
+        for(String s : exprs.split(" ")){
+            if(s.equalsIgnoreCase("(")){
+                if(temp.getLeftChild() == null){
+                    //Instantiate right child, pass in reference to it's parent
+                    temp.setLeftChild(new BinaryNode(temp));
+                    temp = temp.getLeftChild();
+                }
+                else{
+                    //Instantiate right child, pass in reference to it's parent
+                    temp.setRightChild(new BinaryNode(temp));
+                    temp = temp.getRightChild();
+                }
+            }
+            else if(s.equalsIgnoreCase(")")){
+                temp = temp.getParent();
+            }
+
+            //IF AN OPERATOR:    (AND == *, OR == +, NOT == ')
+            else if(s.equalsIgnoreCase("+")){
+                temp.setElement("+");
+            }
+            else if(s.equalsIgnoreCase("*")){
+                temp.setElement("*");
+            }
+            else if(s.equalsIgnoreCase("'")){
+                temp.setElement("'");
+            }
+
+
+            else{
+                //IF GateID
+                if(temp.getLeftChild() == null){
+                    temp.setLeftChild(new BinaryNode(temp));
+                    temp.getLeftChild().setElement(s);
+                }
+                else{
+                    if(temp.getRightChild() == null){
+                        temp.setRightChild(new BinaryNode(temp));
+                        temp.getRightChild().setElement(s);
+                    }
+                }
+            }
+        }
+
+        tree.evaluateTree();
+        //System.out.println("Board output gateID: "+exprsOutput);
+        return tree.getBoard();
+    }
 
     public LogicBoard buildTree(String exprs, String[] inputs, String boardName){
         BoardTree<String> tree = new BoardTree<String>(inputs,boardName);
@@ -45,7 +98,6 @@ public class BoardTreeBuilder {
 
             else{
                 //IF GateID
-                System.out.println("Char: "+s);
                 if(temp.getLeftChild() == null){
                     temp.setLeftChild(new BinaryNode(temp));
                     temp.getLeftChild().setElement(s);
